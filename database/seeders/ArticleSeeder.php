00c89662122d00c136f0a814aaeeb86f79a3ebac
FaizaNA07 <faizaabida44614@gmail.com>
2021-09-28 13:38:00 +0700

    article

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('article')->insert([
+            'title' => 'Belajar Model Dengan Laravel',
+            'image' => 'https://via.placeholder.com/640x480.png/00ff77?text=Belajar Model',
+            'content' => 'Belajar Laravel itu menyenangkan',
+            ]);
+            \App\Models\Post::factory()->count(10)->create();
+    }
+}
